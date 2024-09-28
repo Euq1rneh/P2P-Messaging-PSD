e3cedee9febac346acfaed76473a1a732bca69c0
Henrique Marques <fc57153@alunos.fc.ul.pt>
2024-09-28 13:16:35 +0100

    Added documentation to some methods

--- Peer/Network/ConnectionManager.java
+++ Peer/Network/ConnectionManager.java
@@ -16,6 +16,12 @@ public class ConnectionManager {
         }
     }
 
+    /**
+     * Creates a socket connecting to another peer
+     * @param address the ip address
+     * @param port the port
+     * @return the socket connecting to the peer or null if there was an error
+     */
     private static Socket peer_client(String address, int port){
         try {
             return new Socket(address, port);
@@ -25,6 +31,10 @@ public class ConnectionManager {
         }
     }
 
+    /**
+     * Closes the socket passed as an argument
+     * @param socket the socket to close
+     */
     public static void close_socket(Socket socket){
         if(socket.isClosed()){
             return;
@@ -36,17 +46,25 @@ public class ConnectionManager {
         }
     }
 
+    /**
+     * Tries to establish a connection to a peer
+     * @param peer_address the ip address of the peer
+     * @param peer_port the port of the peer
+     * @return the socket that allows communication with the peer or null if there was an error
+     */
     public static Socket try_connect_to_peer(String peer_address, int peer_port){
         System.out.printf("Trying to connect to %s:%d\n", peer_address, peer_port);
         return peer_client(peer_address, peer_port);
     }
 
-
+    /**
+     * Sends a packet to a peer with the specified socket
+     * @param packet the packet to send
+     * @param socket the socket used for packet transmission
+     */
     public static void sendPacket(Packet packet, Socket socket) {
         try {
-            // Create an ObjectOutputStream to send serialized Packet objects
             ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-            // Write the Packet object to the output stream
             out.writeObject(packet);
             // Flush the stream to make sure the data is sent
             out.flush();
@@ -56,8 +74,8 @@ public class ConnectionManager {
             System.out.println("Error sending packet: " + e.getMessage());
         } finally {
             try {
-                // Close the socket once the packet is sent
-                socket.close(); // NEED TO BE CAREFUL BECAUSE BOTH SENDER AND READER CLOSE THE SOCKET WHICH GENERATES EXCEPTION
+                // NEED TO BE CAREFUL BECAUSE BOTH SENDER AND READER CLOSE THE SOCKET WHICH GENERATES EXCEPTION
+                socket.close();
             } catch (IOException e) {
                 System.out.println("Error closing socket: " + e.getMessage());
             }
--- Peer/Peer.java
+++ Peer/Peer.java
@@ -23,14 +23,20 @@ public class Peer {
         this.out_port = out_port;
     }
 
+    /**
+     * Sends the specified message to the specified peer (address + port)
+     * @param msg the message to send
+     * @param peer_address the ip address of the peer that should receive the message
+     * @param peer_port the port of the peer that should receive the message
+     * @return 0 if there was no error -1 otherwise
+     */
     public int send_message(String msg, String peer_address, int peer_port){
         if(msg == null || msg.isEmpty() || msg.isBlank()){
             System.out.println("No message was provided");
             return -1;
         }
 
-
-
+        //############### This logic should be in the open_conversation method #######################
         //should check if the ip and port match if so there is no need to disconect
         if(peer_out != null && peer_out.isConnected()){
             try {
@@ -55,7 +61,15 @@ public class Peer {
         return 0;
     }
 
+    /**
+     * Open a conversation with a peer if a conversation does not exist it creates a new one.
+     * This method is also responsible for connecting to the peer?????
+     * @param conversation_id the id of the conversation to open
+     * @return 0 if could open the conversation -1 in case of error
+     */
     public int open_conversation(int conversation_id){
+        //should connection only be established when sending a message ???????
+
         return -1;
     }
 
@@ -63,6 +77,9 @@ public class Peer {
 
     }
 
+    /**
+     * Starts the peer by opening a socket that is responsible for accepting connections and reading the incoming messages
+     */
     public void start(){
         // Create a thread to handle accepting connections
         Thread connectionAcceptorThread = new Thread(new Runnable() {
