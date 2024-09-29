77243937f5f53d938be68792de741b5daca7ec23
Henrique Marques <fc57153@alunos.fc.ul.pt>
2024-09-29 10:14:13 +0100

    Refactoring project structure

--- Peer/Data/PacketType.java
+++
@@ -1,7 +0,0 @@
-package Peer.Data;
-
-public enum PacketType {
-    MSG,    //normal message
-    ACK,    //MSG acknowledgment
-    CLS,    //close
-}
--- README.md
+++
@@ -1,8 +0,0 @@
-# P2P-Messaging-PSD
-Projeto de PSD
-
-## Commands
-  - :q - quit
-  - :c - test connection
-  - :m ip port - send message
-
